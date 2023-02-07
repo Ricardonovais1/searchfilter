de048328e5b787091edf549ef653307f5987bc06
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-02-07 20:24:30 -0300

    First Component with styling

--- src/components/ByDecade.js
+++ src/components/ByDecade.js
@@ -1,27 +1,66 @@
 import React from 'react'
+import './ByDecade.css';
+import { useRef } from 'react';
 
-const ByDecade = ({ decade, setDecade, handleSubmit, handleButtonClick, clicked, error, names }) => {
+const ByDecade = ({ decade,
+                    setDecade,
+                    handleSubmit,
+                    handleButtonClick,
+                    clicked,
+                    error,
+                    names
+                }) => {
+
+    const inputGeneral = useRef(null);
 
     return (
     <div>
-        <h1>Ranking de nomes por década</h1>
-        <p>Insira a década que deseja consultar:</p>
+        <header>
+            <h1>Ranking de nomes por década</h1>
+            <p>Consulte por década (1930 - 2010):</p>
+        </header>
+        <body>
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Insira a década" value={decade} onChange={e => setDecade(e.target.value)} />
-            <button type="submit" onClick={handleButtonClick}>Buscar</button>
+            <input
+                className='nameInput'
+                type="text"
+                placeholder="Insira a década"
+                value={decade}
+                onChange={e => setDecade(e.target.value)}
+                ref={inputGeneral}
+            />
+            <button
+                className='inputButton'
+                type="submit"
+                onClick={handleButtonClick}>Buscar
+            </button>
         </form>
-        {clicked && <p>Ranking de nomes para a década de {decade}</p>}
+        {clicked && <p className='tableTitle'>Ranking de nomes para a década de {decade}</p>}
         {error ? (
         <div>{error}</div>
         ) : (
             <div className='namesContainer'>
-            <ul className="names">
-                {names.map((item, index) => (
-                <li key={index}>{`${item.ranking} - ${item.nome} - Ocorrências: ${item.frequencia}`}</li>
-                ))}
-            </ul>
+
+                <table align='center'>
+                {clicked &&
+                    <tr>
+                        <th align='left'>Ranking</th>
+                        <th align='left'>Nome</th>
+                        <th align='left'>Ocorrências</th>
+                    </tr>
+                }
+                    {names.map((item, index) => (
+                    <tr key={index}>
+                        <td align='left'>{item.ranking}º</td>
+                        <td align='left'>{item.nome}</td>
+                        <td align='left'>{item.frequencia}</td>
+                    </tr>
+                    ))}
+                </table>
             </div>
         )}
+        </body>
+
     </div>
   )
 }
